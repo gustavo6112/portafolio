3dbd50075c7482cc54dbdf3002419bfd272409dd
Luis Gustavo Flores Avapori <Gustavo6112@gmail.com>
2019-05-15 11:09:47 -0400

    about cargar los tres personaje

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Equipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,16 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info:InfoPagina;//para usar esto primero tiene que crear su interfas
   cargada:boolean=false;
+  equipo:Equipo;
   constructor(private http:HttpClient) {//con el servicio en el constructor puedo realizar pedido a servidores externos y mas
-    console.log('servicio de info pagina listo');
+    //console.log('servicio de info pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+  }
 
-
-    //Leer el archivo JSON
+  private cargarInfo(){
+    //Leer el archivo JSON que esta en la carpeta assest en el proyecto
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp:InfoPagina)=>{
         this.cargada=true;
@@ -22,5 +28,15 @@ export class InfoPaginaService {
       //para recuperar la respuesta solo un atributo resp.twitter
       //resolver el error si solo ponemos para sacar el dato (resp:any)=>{col.log(res.twitter)}
       //resolver el error si solo ponemos para sacar el dato console.log(resp['twitter'])
+  }
+
+  private cargarEquipo(){
+    //Leer el archivo json desde la api firebase de google
+     this.http.get('https://angular-html-acbee.firebaseio.com/equipo.json')
+      .subscribe((resp:Equipo)=>{
+        this.equipo=resp;
+        this.cargada=true;
+        console.log(resp);
+      });
   }
 }
